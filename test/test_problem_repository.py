38889ab928f035c6d0f3924a6d9cc1a28adf41a6
asato <asato@sabira.jp>
2023-06-26 17:54:32 +0900

    add

--- test/test_problem_repository.py
+++ test/test_problem_repository.py
@@ -3,40 +3,44 @@ import unittest
 import sys
 sys.path.append("..")
 from app.repositories.problem_repository import ProblemRepository
+from app.models.problem import Problem
 from app.types.problem import ProblemInfo, Constants
 from resources.data.problem import ProblemData
 
 class TestProblemRepository(unittest.TestCase):
     def setUp(self):
-        self.skipTest("depends on db")
+        # self.skipTest("depends on db")
         pass
 
     def test_crud_front_end_problem(self):
         # arrange
         problem_info = ProblemData.front_end_coding
+        problem = Problem(problem_info.problemCd)
 
         # action
-        ProblemRepository.regist(problem_info)
-        ProblemRepository.save(problem_info) 
-        ProblemRepository.delete(problem_info)
+        ProblemRepository.regist(problem)
+        ProblemRepository.save(problem) 
+        ProblemRepository.delete(problem)
 
-    # @unittest.skip("temporary test")
+    @unittest.skip("temporary test")
     def test_get_by_problem_cd(self):
         # arrange
         problem_info = ProblemData.front_end_coding
+        problem = Problem(problem_info.problemCd)
 
         # action
-        ProblemRepository.regist(problem_info)
+        ProblemRepository.regist(problem)
         problem = ProblemRepository.find_by_problem_cd(problem_info.problemCd)
 
         # assert
-        expected = problem_info.title
-        actual = problem.get_title()
+        expected = problem_info.problemCd
+        actual = problem.get_problem_cd()
         self.assertEqual(expected, actual)
 
         # after
         ProblemRepository.delete(problem_info)
 
+    @unittest.skip("temporary test")
     def test_get_all_problems(self):
         # arrange
         problem_info1 = ProblemData.front_end_coding
